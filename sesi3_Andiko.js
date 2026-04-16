e2909383bb4db7abdd5989e8eea52578e7e65c2d
Ddyko <andiko.gunawan@gmail.com>
2026-04-16 18:28:04 +0700

    adding task files required

---
+++ sesi3_Andiko.js
@@ -0,0 +1,13 @@
+let i = 1;
+let star = "*";
+while (i < 5){
+    console.log(star);
+    star += "*"
+    i++
+}
+
+// let starArray = ["*","**","***","****"] //Cara Bodoh
+
+// starArray.forEach(function(bintang){
+//     console.log(bintang)
+// })
